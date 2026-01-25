94defb7777130f34fd370fb60be94ccea2c13a18
naila-saleh <nailasaleh2004@gmail.com>
2026-01-25 22:34:15 +0200

    add SPA

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../components/navbar/Navbar.jsx";
+import {Outlet} from "react-router-dom";
+import Footer from "../components/footer/Footer.jsx";
+
+export default function MainLayout() {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
---
+++ src/router.jsx
@@ -0,0 +1,27 @@
+import {createBrowserRouter} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.jsx";
+import About from "./pages/about/About.jsx";
+import Home from "./pages/home/Home.jsx";
+import Users from "./pages/users/Users.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: "about",
+                element: <About />
+            },
+            {
+                path: "users",
+                element: <Users />
+            }
+        ]
+    }
+]);
+export default router;
